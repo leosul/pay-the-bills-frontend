a5b328dd42e6d9e639cb041cb426522885fe558c
Leonardo <leosul@gmail.com>
2019-12-08 20:37:09 +0000

    Basic menu

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2019
+            <a href='leosul@icloud.com' target='_blank'> Leonardo Jacques da Silva</a>
+        </strong>
+    </footer>
+)
